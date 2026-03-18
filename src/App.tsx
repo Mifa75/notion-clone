6ae3b5d769aaaa9f85857dfeb3847d2ca225ec70
Silvia Scano <scanosilvia75@gmail.com>
2026-03-18 11:27:19 +0000

    Create Cover component and add styling files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import { Cover } from "./Page/Cover";
 
 function App() {
-  return <>Notion clone</>;
+  return (
+    <>
+      <Cover />
+    </>
+  );
 }
 
 export default App;
